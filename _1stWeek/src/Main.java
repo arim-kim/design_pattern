9e41831f3d585b9e8b6df310d4cd3ee3a17e27f6
arimkim <kal72222222@gmail.com>
2023-03-17 10:45:13 +0900

    initial commit

--- _1stWeek/src/Main.java
+++ _1stWeek/src/Main.java
@@ -1,7 +1,88 @@
 public class Main {
     public static void main(String[] args) {
 
-
-        System.out.println("Hello");
+        BookShelf bookShelf = new BookShelf(0);
+        bookShelf.appendBook("가");
+        bookShelf.appendBook("나");
+        bookShelf.appendBook("다");
+        bookShelf.appendBook("라");
+        BookShelfIterator bookShelfIterator = new BookShelfIterator(0,bookShelf);
+        while(bookShelfIterator.hasNext()){
+            System.out.println(((Book)bookShelfIterator.next()).getName());
+        }
     }
 }
+
+interface Iterator {
+    boolean hasNext();
+    Object next();
+}
+
+interface Aggregate {
+    Iterator iterator();
+}
+
+class BookShelf implements Aggregate {
+    Book[] books = {};
+    int last;
+
+    public BookShelf(int last) {
+        this.last = last;
+    }
+
+    Object getBookAt(int i) {
+        return books[i];
+    }
+
+    void appendBook(String name) {
+        books[last] = new Book(name);
+        last++;
+    }
+
+    int getLength() {
+        return books.length;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new BookShelfIterator(0, new BookShelf( this.last));
+    }
+}
+class BookShelfIterator implements Iterator{
+
+    int index = 0;
+    BookShelf bookShelf;
+
+    public BookShelfIterator(int index, BookShelf bookShelf) {
+        this.index = index;
+        this.bookShelf = bookShelf;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return (index < bookShelf.getLength());
+    }
+
+    @Override
+    public Object next() {
+        Book book = ((Book)bookShelf.getBookAt(index));
+        index ++;
+        return book;
+    }
+}
+class Book {
+    String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+
+
+
+
